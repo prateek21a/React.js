499590f5ff489990ce976f078d977d69191fbde4
Prateek Jaiswal <pjbabu21a@gmail.com>
2025-02-17 16:27:31 +0530

    Counter_Project-01

---
+++ Counter_Project-01/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+ let [counter , setCounter]= useState(0)
+
+  const addValue = () => {
+    if ( counter < 20){
+      setCounter(counter + 1)
+    }
+else (
+  "Not going beyond 20"
+)
+  }
+  const removeValue = () => {
+    if( counter > 0 ){
+      setCounter(counter-1);
+    } else {
+      "not going below 0 "
+    }
+  
+  }
+
+  return (
+    <>
+ <h1> Chai Or React </h1>
+ <h1> Counter value : {counter} </h1>
+
+ <button
+ onClick={addValue}>Add Value</button>
+ <br/>
+ <button onClick={removeValue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
