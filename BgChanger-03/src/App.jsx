a71e9e2b8ac421017f3904d1fbd9305c8cf0acf5
Prateek Jaiswal <pjbabu21a@gmail.com>
2025-02-19 15:29:49 +0530

    cursor-pointer added

--- BgChanger-03/src/App.jsx
+++ BgChanger-03/src/App.jsx
@@ -7,66 +7,64 @@ function App() {
     <div className='bg-gray-200 h-screen flex items-center justify-center'
     style={{backgroundColor: color}}>
 
-<div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
           <button
-          onClick={() => Setcolor("red")}
-          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-          style={{backgroundColor: "red"}}
+            onClick={() => Setcolor("Red")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg cursor-pointer"
+            style={{backgroundColor: "red"}}
           >Red</button>
           
-           <button
-          onClick={() => Setcolor("green")}
-          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-          style={{backgroundColor: "green"}}
+          <button
+            onClick={() => Setcolor("green")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg cursor-pointer"
+            style={{backgroundColor: "green"}}
           >Green</button>
 
           <button
-          onClick={() => Setcolor("blue")}
-          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-          style={{backgroundColor: "blue"}}
+            onClick={() => Setcolor("blue")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg cursor-pointer"
+            style={{backgroundColor: "blue"}}
           >Blue</button>
 
-            <button
-          onClick={() => Setcolor("Yellow")}
-          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-          style={{backgroundColor: "yellow"}}
+          <button
+            onClick={() => Setcolor("Yellow")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg cursor-pointer"
+            style={{backgroundColor: "yellow"}}
           >Yellow</button>
 
-            <button
-          onClick={() => Setcolor("Violet")}
-          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-          style={{backgroundColor: "violet"}}
+          <button
+            onClick={() => Setcolor("Violet")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg cursor-pointer"
+            style={{backgroundColor: "violet"}}
           >Violet</button>
 
-            <button
-          onClick={() => Setcolor("Purple")}
-          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-          style={{backgroundColor: "purple"}}
+          <button
+            onClick={() => Setcolor("Purple")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg cursor-pointer"
+            style={{backgroundColor: "purple"}}
           >Purple</button>
 
-            <button
-          onClick={() => Setcolor("Brown")}
-          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-          style={{backgroundColor: "brown"}}
+          <button
+            onClick={() => Setcolor("Brown")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg cursor-pointer"
+            style={{backgroundColor: "brown"}}
           >Brown</button>
 
-            <button
-          onClick={() => Setcolor("Pink")}
-          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-          style={{backgroundColor: "Pink"}}
+          <button
+            onClick={() => Setcolor("Pink")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg cursor-pointer"
+            style={{backgroundColor: "Pink"}}
           >Pink</button>
 
-            <button
-          onClick={() => Setcolor("grey")}
-          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-          style={{backgroundColor: "grey"}}
+          <button
+            onClick={() => Setcolor("grey")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg cursor-pointer"
+            style={{backgroundColor: "grey"}}
           >Grey</button>
         </div>
       </div>
     </div>
-        
-        
   )
 }
 
